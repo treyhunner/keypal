eb5cb4a44b1b229f6c2389c110b6f46c3731583c
Trey Hunner <trey@treyhunner.com>
2026-05-06 20:12:24 -0700

    Add tests for models and pack loader

---
+++ tests/test_models.py
@@ -0,0 +1,76 @@
+from keypal.models import Pack, Shortcut, load_pack, parse_pack
+
+
+def test_parse_pack_minimal():
+    data = {
+        "pack": {
+            "id": "test",
+            "name": "Test Pack",
+            "description": "A test pack",
+        },
+        "shortcuts": [
+            {"action": "Do thing", "keys": ["ctrl+a"]},
+        ],
+    }
+    pack = parse_pack(data)
+    assert pack == Pack(
+        id="test",
+        name="Test Pack",
+        description="A test pack",
+        shortcuts=(Shortcut(action="Do thing", keys=("ctrl+a",)),),
+    )
+
+
+def test_parse_pack_optional_fields():
+    data = {
+        "pack": {"id": "t", "name": "T", "description": "d"},
+        "shortcuts": [
+            {
+                "action": "Open menu",
+                "keys": ["f10", "alt+space"],
+                "tags": ["nav"],
+                "hint": "Top of screen",
+                "capturable": False,
+            },
+        ],
+    }
+    pack = parse_pack(data)
+    [shortcut] = pack.shortcuts
+    assert shortcut.keys == ("f10", "alt+space")
+    assert shortcut.tags == ("nav",)
+    assert shortcut.hint == "Top of screen"
+    assert shortcut.capturable is False
+
+
+def test_parse_pack_no_shortcuts():
+    data = {"pack": {"id": "empty", "name": "E", "description": "d"}}
+    pack = parse_pack(data)
+    assert pack.shortcuts == ()
+
+
+def test_load_pack_from_file(tmp_path):
+    toml = """
+[pack]
+id = "x"
+name = "X"
+description = "x desc"
+
+[[shortcuts]]
+action = "Foo"
+keys = ["ctrl+f"]
+"""
+    path = tmp_path / "x.toml"
+    path.write_text(toml)
+    pack = load_pack(path)
+    assert pack.id == "x"
+    assert pack.shortcuts[0].action == "Foo"
+
+
+def test_shortcut_id_format():
+    pack = Pack(
+        id="readline",
+        name="n",
+        description="d",
+        shortcuts=(Shortcut(action="Move to start", keys=("ctrl+a",)),),
+    )
+    assert pack.shortcut_id(pack.shortcuts[0]) == "readline:Move to start"
